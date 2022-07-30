1e162c4413af598069bce8d5ba663cb9a2eceb97
Jimmy Ayivor <12377575+JimmyAyivor@users.noreply.github.com>
2022-07-30 19:15:23 -0400

    Update transactions.models.js

--- models/transactions.models.js
+++ models/transactions.models.js
@@ -3,22 +3,22 @@ module.exports = [
       item_name: "Income",
       amount: 250000,
       date: "Jan 1",
-      from: "employer",
-      category: "Taxes",
+      from: "Employer",
+      category: "Income",
     },
     {
         item_name: "Taxes",
         amount: 1,
         date: "May 2",
-        from: "employer",
+        from: "IRS",
         category: "Taxes",
       },
       {
         item_name: "Retirement",
         amount: 20000000,
         date: "Jun 3",
-        from: "employer",
-        category: "Taxes",
+        from: "Me",
+        category: "Savings & Investments",
       },
       
   ];
